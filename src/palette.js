dcd07bddcbe0169414d6b9bd9a543c510e76e8f7
orstavik <orstavik77@gmail.com>
2025-04-09 22:09:43 +0200

    bugfixes ,must read carefully

--- src/palette.js
+++ src/palette.js
@@ -19,7 +19,7 @@ import { borderSwitch, default as NativeFunctions, toLogicalFour } from "./func.
   caret-color: inherit;
   accent-color: inherit;
   text-emphasis-color: inherit;
-  text-decoration-color: inherit;
+  text-decoration-color: inherit; //there is a bug here, chrome goes to shortcut message 
   column-rule-color: inherit;
   outline-color: inherit;
 }
@@ -121,10 +121,10 @@ const funcs = {
   caret: a => ({ "caret-color": a }),
   accent: a => ({ "accent-color": a }),
   emphasis: a => ({ "text-emphasis-color": a }),
-  decoration: a => ({ "text-decoration-color": a }),
   column: a => ({ "column-rule-color": a }),
   outline: a => ({ "outline-color": a }),
-  border: (...args) => borderSwitch(toLogicalFour("border-color", ...args)),
+  border: (...args) => borderSwitch(toLogicalFour("Color", ...args)),
+  decoration: a => ({ "--text-decoration-color": a }),
   shadow: (box, text = box, drop = box) => ({
     "--box-shadow-color": box,
     "--text-shadow-color": text,
@@ -136,37 +136,74 @@ for (const fn of Object.values(funcs))
 
 color.scope = { ...ColorScope, ...funcs };
 
-
-
-
-function boxShadow(...args) {
-  if (args.every(a => isLength(a) || a === "inset"))
-    args.push(`var(--box-shadow-color, oklch(from currentcolor l 0 h))`);
-  return NativeCssProperties.boxShadow(...args);
-};
-function textShadow(...args) {
-  if (args.every(a => isLength(a)))
-    args.push(`var(--text-shadow-color, oklch(from currentcolor l 0 h))`);
-  return NativeCssProperties.boxShadow(...args);
+const textDecorationColor = "var(--text-decoration-color, currentcolor)";
+function textDecoration(textDecorationLine, textDecorationThickness) {
+  return { textDecorationLine, textDecorationThickness, textDecorationColor };
 }
-function dropShadow(...args) {
-  if (args.every(a => isLength(a)))
-    args.push(`var(--drop-shadow-color, oklch(from currentcolor l 0 h))`);
-  return NativeCssProperties.boxShadow(...args);
+function textDecorationStyle(textDecorationStyle, textDecorationLine, textDecorationThickness) {
+  return { textDecorationLine, textDecorationThickness, textDecorationStyle, textDecorationColor };
 }
 
-boxShadow.scope = ColorScope;
-textShadow.scope = ColorScope;
-dropShadow.scope = ColorScope;
+const textDecorations = {
+  dashedOverLine: textDecorationStyle.bind(null, "dashed", "overline"),
+  dashedOverLineThrough: textDecorationStyle.bind(null, "dashed", "overline line-through"),
+  dashedOverUnderLine: textDecorationStyle.bind(null, "dashed", "overline underline"),
+  dashedOverUnderLineThrough: textDecorationStyle.bind(null, "dashed", "overline underline line-through"),
+  dashedLineThrough: textDecorationStyle.bind(null, "dashed", "line-through"),
+  dashedUnderLine: textDecorationStyle.bind(null, "dashed", "underline"),
+  dashedUnderLineThrough: textDecorationStyle.bind(null, "dashed", "underline line-through"),
+  dottedOverLine: textDecorationStyle.bind(null, "dotted", "overline"),
+  dottedOverLineThrough: textDecorationStyle.bind(null, "dotted", "overline line-through"),
+  dottedOverUnderLine: textDecorationStyle.bind(null, "dotted", "overline underline"),
+  dottedOverUnderLineThrough: textDecorationStyle.bind(null, "dotted", "overline underline line-through"),
+  dottedLineThrough: textDecorationStyle.bind(null, "dotted", "line-through"),
+  dottedUnderLine: textDecorationStyle.bind(null, "dotted", "underline"),
+  dottedUnderLineThrough: textDecorationStyle.bind(null, "dotted", "underline line-through"),
+  doubleOverLine: textDecorationStyle.bind(null, "double", "overline"),
+  doubleOverLineThrough: textDecorationStyle.bind(null, "double", "overline line-through"),
+  doubleOverUnderLine: textDecorationStyle.bind(null, "double", "overline underline"),
+  doubleOverUnderLineThrough: textDecorationStyle.bind(null, "double", "overline underline line-through"),
+  doubleLineThrough: textDecorationStyle.bind(null, "double", "line-through"),
+  doubleUnderLine: textDecorationStyle.bind(null, "double", "underline"),
+  doubleUnderLineThrough: textDecorationStyle.bind(null, "double", "underline line-through"),
+  wavyOverLine: textDecorationStyle.bind(null, "wavy", "overline"),
+  wavyOverLineThrough: textDecorationStyle.bind(null, "wavy", "overline line-through"),
+  wavyOverUnderLine: textDecorationStyle.bind(null, "wavy", "overline underline"),
+  wavyOverUnderLineThrough: textDecorationStyle.bind(null, "wavy", "overline underline line-through"),
+  wavyLineThrough: textDecorationStyle.bind(null, "wavy", "line-through"),
+  wavyUnderLine: textDecorationStyle.bind(null, "wavy", "underline"),
+  wavyUnderLineThrough: textDecorationStyle.bind(null, "wavy", "underline line-through"),
 
+  blink: textDecoration.bind(null, "blink"),
+  overLine: textDecoration.bind(null, "overline"),
+  overLineThrough: textDecoration.bind(null, "overline line-through"),
+  overUnderLine: textDecoration.bind(null, "overline underline"),
+  overUnderLineThrough: textDecoration.bind(null, "overline underline line-through"),
+  lineThrough: textDecoration.bind(null, "line-through"),
+  underLine: textDecoration.bind(null, "underline"),
+  underLineThrough: textDecoration.bind(null, "underline line-through"),
+  grammarError: textDecoration.bind(null, "grammar-error"),
+  spellingError: textDecoration.bind(null, "spelling-error"),
+}
 
-const colorfulShorts = {
+//Override the text-emphasis and column-rule and outline and accent and caret needs to be updated.
+//So that they split the inputs into different values, and doesn't touch colors.
+
+const border = (function () {
+  const cb = NativeFunctions.border.bind();
+  const { c, color, ...restScope } = NativeFunctions.border.scope;
+  cb.scope = restScope;
+  return cb;
+})();
+
+export default {
   palette,
   darkPalette,
   color,
-  boxShadow,
-  textShadow,
-  dropShadow,
+  boxShadow: (...args) => [...args, `var(--box-shadow-color, oklch(from currentcolor l 0 0))`].join(" "),
+  boxShadowInset: (...args) => ["inset", ...args, `var(--box-shadow-color, oklch(from currentcolor l 0 0))`].join(" "),
+  textShadow: (...args) => [...args, `var(--text-shadow-color, oklch(from currentcolor l 0 0))`].join(" "),
+  dropShadow: (...args) => [...args, `var(--drop-shadow-color, oklch(from currentcolor l 0 0))`].join(" "),
+  border,
+  ...textDecorations,
 };
-
-export default colorfulShorts;
