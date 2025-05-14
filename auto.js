66fffaa11ef96bf9f5c524ad753d10f6752765ff
Lae Nicole <lmevangelista1@up.edu.ph>
2025-05-14 19:34:44 +0800

    added auto for testing in codepen

---
+++ auto.js
@@ -0,0 +1,27 @@
+import { SheetWrapper } from "./src/engine.js";
+
+function initCssShorts(style, interval = 500) {
+  style || document.head.appendChild(style = document.createElement("style"));
+  const sheetWrapper = new SheetWrapper(style.sheet);
+
+  function update() {
+    for (let el of document.body.querySelectorAll('[csss]'))
+      try { sheetWrapper.readSupers(el.getAttribute("csss")); } catch (er) { console.error(er); }
+    for (let el of document.body.querySelectorAll('[class*="$"]'))
+      for (let clazz of el.classList)
+        if (clazz.includes("$"))
+          try { sheetWrapper.addRule(clazz); } catch (er) { console.error(er); }
+    sheetWrapper.cleanup();
+  }
+  if (interval < 0)
+    return requestAnimationFrame(update);
+  return setInterval(update, interval);
+}
+let style, interval;
+if (import.meta.url) {
+  const sp = new URL(import.meta.url).searchParams;
+  interval = sp.get("interval");
+  let style = sp.get("style");
+  if (style) style = document.querySelector(style);
+}
+initCssShorts(style, interval);
