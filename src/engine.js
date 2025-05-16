6bd7123d1b038e4b743b607c243098aa40c38d25
orstavik <orstavik77@gmail.com>
2025-05-16 21:19:17 +0200

    bs

--- src/engine.js
+++ src/engine.js
@@ -2,9 +2,70 @@ import { ShortBlock } from "./Parser.js";
 import nativeAndMore from "./func.js";
 import layouts from "./layout.js";
 import colorPalette from "./palette.js";
-import { MEDIA_WORDS } from "./mediaFuncs.js";
 
-const SHORTS = {};
+const MEDIA_WORDS = {
+  progressive: "scan: progressive",
+  interlace: "scan: interlace",
+  dim: "light-level: dim",
+  normalLight: "light-level: normal",
+  washed: "light-level: washed",
+  reducedData: "prefers-reduced-data: reduce",
+  noReducedData: "prefers-reduced-data: no-preference",
+  noScript: "scripting: none",
+  initScript: "scripting: initial-only",
+  reloadScript: "scripting: reload",
+  stableScript: "scripting: stable",
+
+  dark: "prefers-color-scheme: dark",
+  light: "prefers-color-scheme: light",
+  noColorScheme: "prefers-color-scheme: no-preference",
+  portrait: "orientation: portrait",
+  landscape: "orientation: landscape",
+  highContrast: "prefers-contrast: high",
+  lowContrast: "prefers-contrast: low",
+  forcedContrast: "prefers-contrast: forced",
+  reducedTransparency: "prefers-reduced-transparency: reduce",
+  noReducedTransparency: "prefers-reduced-transparency: no-preference",
+  forcedColors: "forced-colors: active",
+  noForcedColors: "forced-colors: none",
+  invertedColors: "inverted-colors: inverted",
+  noInvertedColors: "inverted-colors: none",
+  p3: 'color-gamut: p3',
+  srgb: 'color-gamut: srgb',
+  rec2020: 'color-gamut: rec2020',
+  highDynamicRange: "dynamic-range: high",
+  standardDynamicRange: "dynamic-range: standard",
+  reducedMotion: "prefers-reduced-motion: reduce",
+  noReducedMotion: "prefers-reduced-motion: no-preference",
+  standalone: "display-mode: standalone",
+  fullscreen: "display-mode: fullscreen",
+  minimalUi: "display-mode: minimal-ui",
+  browser: "display-mode: browser",
+  windowControlsOverlay: "display-mode: window-controls-overlay",
+  pip: "display-mode: picture-in-picture",
+  slowUpdate: "update: slow",
+  fastUpdate: "update: fast",
+  noUpdate: "update: none",
+  hover: "hover: hover",
+  noHover: "hover: none",
+  coarsePointer: "pointer: coarse",
+  finePointer: "pointer: fine",
+  noPointer: "pointer: none",
+  grid: "grid: 1",
+  bitmap: "grid: 0",
+  anyHover: "any-hover: hover",
+  anyNoHover: "any-hover: none",
+  anyCoarsePointer: "any-pointer: coarse",
+  anyFinePointer: "any-pointer: fine",
+  anyNoPointer: "any-pointer: none",
+  anyGrid: "any-grid: 1",
+  anyBitmap: "any-grid: 0",
+  screen: "screen",
+  print: "print",
+  all: "all",
+}
+
+// const SHORTS = {};
 
 // todo make this work with @media.
 // | Feature                  | Possible values                             | Example                                             |
@@ -18,6 +79,7 @@ const RENAME = {
 
 class UpgradeRegistry {
   #register = {};
+  #shorts = {}
 
   constructor(dict) {
     for (const [k, short] of Object.entries(dict))
@@ -42,7 +104,7 @@ class UpgradeRegistry {
 
   registerShort(name, func) {
     const [main, name2 = main] = name.split(".");
-    const table = name2 != main ? SHORTS[main].scope ??= {} : SHORTS;
+    const table = name2 != main ? this.#shorts[main].scope ??= {} : this.#shorts;
     if (name2 in table)
       throw new Error(`Short name ${name} already exists`);
     table[name2] = func;
@@ -50,7 +112,7 @@ class UpgradeRegistry {
   }
 
   get shorts() {
-    return SHORTS;
+    return this.#shorts;
   }
 
   get medias() {
@@ -100,7 +162,6 @@ class SheetWrapper {
     this.sheet = sheet;
     this.items = this.setupLayer("items", sheet);
     this.container = this.setupLayer("container", sheet);
-    this.shorts = { ...SHORTS };
     this.setupStatement();
   }
 
