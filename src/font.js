96544f257592bcd477b0b0cab0177a2562799ce0
orstavik <orstavik77@gmail.com>
2025-08-09 21:31:55 +0200

    bs

--- src/font.js
+++ src/font.js
@@ -1,19 +1,7 @@
 import { isLength } from "./func.js";
 
-const FONT_RESET = {
-  fontFamily: `var(--fontFamily, unset)`,
-  fontSize: `var(--fontSize, 1rem)`,
-  fontStyle: `var(--fontStyle, unset)`,
-  fontWeight: `var(--fontWeight, unset)`,
-  fontVariantCaps: `var(--fontVariantCaps, unset)`,
-  fontStretch: `var(--fontStretch, unset)`,
-  fontSynthesis: `var(--fontSynthesis, unset)`,
-  fontSizeAdjust: `var(--fontSizeAdjust, unset)`,
-  letterSpacing: `var(--letterSpacing, unset)`,
-};
-
-const FONT_DEFAULTS = {
-  fontFamily: "unset",
+const FONT_DEFAULTS = Object.freeze({
+  fontFamily: "system-ui, sans-serif",
   fontSize: "1rem",
   fontStyle: "unset",
   fontWeight: "unset",
@@ -22,7 +10,7 @@ const FONT_DEFAULTS = {
   fontStretch: "unset",
   fontSynthesis: "unset",
   letterSpacing: "unset",
-};
+});
 
 const KEYWORDS = {
   bold: { fontWeight: "bold" },
@@ -56,6 +44,8 @@ const KEYWORDS = {
   noKerning: { fontKerning: "none" },
 
   normal: { fontStyle: "normal", fontWeight: "normal" },
+  smooth: { WebkitFontSmoothing: "auto", MozOsxFontSmoothing: "auto" },
+  emoji: { fontEmoji: ", 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'" },
 };
 
 function noSynthesis(a) {
@@ -65,7 +55,8 @@ function noSynthesis(a) {
   return ["Style", "Weight", "SmallCaps", "Position"]
     .filter(a => args.includes(a))
     .join(" ")
-    .replace("Caps", "-caps").toLowerCase();
+    .replace("Caps", "-caps")
+    .toLowerCase();
 }
 
 function nonFontArg(a) {
@@ -79,8 +70,9 @@ function nonFontArg(a) {
   if (fontSynthesis) return { fontSynthesis };
 }
 
-function font(...args) {
-  if (!args.length) return { ...FONT_RESET };
+function fontImpl(...args) {
+  if (!args.length)
+    return Object.fromEntries(Object.entries(FONT_DEFAULTS).map(([k, v]) => [k, `var(--${k}, ${v})`]));
   const res = { ...FONT_DEFAULTS };
   const ff = [];
   for (let a of args) {
@@ -96,7 +88,16 @@ function font(...args) {
   }
   if (ff.length)
     res.fontFamily = ff.join(", ");
+  if (res.fontEmoji) {
+    res.fontFamily += res.fontEmoji;
+    delete res.fontEmoji;
+  }
   return res;
+}
+
+function font(...args) {
+  return fontImpl(...args);
+  //todo add all the --fontVariableName: value
 }
 font.scope = {
   // ...NativeCssProperties.font.scope,
@@ -125,37 +126,52 @@ font.scope = {
 
 //$type(name,...args) => creates a type with the given name and properties.
 //$type(name) => uses a type and sets the font properties to the type's properties.
-function type(...args) {
-  if (!args.length) throw new SyntaxError("The $type() function must always include a name as first argument");
 
+const BUILTIN_TYPES = {
+  transitional: { fontFamily: "Charter,'Bitstream Charter','Sitka Text',Cambria,serif" },
+  oldStyle: { fontFamily: "'Iowan Old Style','Palatino Linotype','URW Palladio L',P052,serif" },
+  humanist: { fontFamily: "Seravek,'Gill Sans Nova',Ubuntu,Calibri,'DejaVu Sans',source-sans-pro,sans-serif" },
+  geometricHumanist: { fontFamily: "Avenir,Montserrat,Corbel,'URW Gothic',source-sans-pro,sans-serif" },
+  classicalHumanist: { fontFamily: "Optima,Candara,'Noto Sans',sans-serif" },
+  neoGrotesque: { fontFamily: "Inter,Roboto,'Helvetica Neue','Arial Nova','Nimbus Sans',Arial,sans-serif" },
+  monospaceSlabSerif: { fontFamily: "'Nimbus Mono PS','Courier New',monospace,", WebkitFontSmoothing: "auto", MozOsxFontSmoothing: "auto" },
+  monospaceCode: { fontFamily: "ui-monospace,'Cascadia Code','Source Code Pro',Menlo,Consolas,'DejaVu Sans Mono',monospace" },
+  industrial: { fontFamily: "Bahnschrift,'DIN Alternate','Franklin Gothic Medium','Nimbus Sans Narrow',sans-serif-condensed,sans-serif" },
+  roundedSans: { fontFamily: "ui-rounded,'Hiragino Maru Gothic ProN',Quicksand,Comfortaa,Manjari,'Arial Rounded MT','Arial Rounded MT Bold',Calibri,source-sans-pro,sans-serif" },
+  slabSerif: {
+    "@fontFace": { fontFamily: "Rockwell,", src: "local('Rockwell')", ascentOverride: "100%" },
+    fontFamily: "Rockwell,'Rockwell Nova','Roboto Slab','DejaVu Serif','Sitka Small',serif",
+  },
+  antique: { fontFamily: "Superclarendon,'Bookman Old Style','URW Bookman','URW Bookman L','Georgia Pro',Georgia,serif" },
+  didone: { fontFamily: "Didot,'Bodoni MT','Noto Serif Display','URW Palladio L',P052,Sylfaen,serif", WebkitFontSmoothing: "auto", MozOsxFontSmoothing: "auto" },
+  handwritten: { fontFamily: "'Segoe Print','Bradley Hand',Chilanka,TSCu_Comic,casual,cursive" },
+};
+
+function makeConstUse(obj, name) {
+  const res = {};
+  for (const [k, v] of Object.entries(obj))
+    res[k] = res["--" + k] = `var(--${name + k[0].toUpperCase() + k.slice(1)}, ${v})`;
+  return res;
+}
+function makeConstDeclaration(obj, name) {
+  const res = {};
+  for (const [k, v] of Object.entries(obj))
+    res["--" + name + k[0].toUpperCase() + k.slice(1)] = v;
+  return res;
+}
+
+function type(...args) {
+  if (!args.length)
+    throw new SyntaxError("The $type() function must always include a name as first argument");
   const name = args[0];
   if (!(typeof name != "string"))
     throw new SyntaxError(`The typename in $type(typename,...args) is not interpreted as a string: "${JSON.stringify(name)}"`);
   if (!name.match(/^[a-z][a-z0-9-]*$/))
     throw new SyntaxError(`The typename in $type(typename,...args) must be lowercase and match(/^[a-z][a-z0-9-]*$/): "${name}"`);
-  if (args.length === 1)
-    return {
-      fontFamily: `var(--${name}FontFamily, unset)`,
-      fontStyle: `var(--${name}FontStyle, unset)`,
-      fontWeight: `var(--${name}FontWeight, unset)`,
-      fontVariantCaps: `var(--${name}FontVariantCaps, unset)`,
-      fontStretch: `var(--${name}FontStretch, unset)`,
-      letterSpacing: `var(--${name}LetterSpacing, unset)`,
-      fontSynthesis: `var(--${name}FontSynthesis, unset)`,
-      "--fontFamily": `var(--${name}FontFamily, unset)`,
-      "--fontStyle": `var(--${name}FontStyle, unset)`,
-      "--fontWeight": `var(--${name}FontWeight, unset)`,
-      "--fontVariantCaps": `var(--${name}FontVariantCaps, unset)`,
-      "--fontStretch": `var(--${name}FontStretch, unset)`,
-      "--letterSpacing": `var(--${name}LetterSpacing, unset)`,
-      "--fontSynthesis": `var(--${name}FontSynthesis, unset)`,
-    };
-  const fs = font(...args.slice(1)); //do we want to rename the font here to type?
-  const res = {};
-  for (const k in fs)
-    if (!k.startsWith("--"))
-      res["--" + name + k[0].toUpperCase() + k.slice(1)] = fs[k];
-  return res;
+
+  return args.length === 1 ?
+    makeConstUse(BUILTIN_TYPES[name] ?? FONT_DEFAULTS, name) :
+    makeConstDeclaration(font(...args), name);
 }
 type.scope = { ...font.scope };
 
@@ -194,4 +210,22 @@ export default {
   ultraExpanded: { Stretch: "ultra-expanded" },
   kerning: { fontKerning: "normal" },
   noKerning: { fontKerning: "none" },
+};
+
+
+const BUILTIN_TYPES2 = {
+  transitional: { fontFamily: "Charter,'Bitstream Charter','Sitka Text',Cambria,serif" },
+  oldStyle: { fontFamily: "'Iowan Old Style','Palatino Linotype','URW Palladio L',P052,serif" },
+  humanist: { fontFamily: "Seravek,'Gill Sans Nova',Ubuntu,Calibri,'DejaVu Sans',source-sans-pro,sans-serif" },
+  geometricHumanist: { fontFamily: "Avenir,Montserrat,Corbel,'URW Gothic',source-sans-pro,sans-serif" },
+  classicalHumanist: { fontFamily: "Optima,Candara,'Noto Sans',sans-serif" },
+  neoGrotesque: { fontFamily: "Inter,Roboto,'Helvetica Neue','Arial Nova','Nimbus Sans',Arial,sans-serif" },
+  monospaceSlabSerif: { fontFamily: "'Nimbus Mono PS','Courier New',monospace," },
+  monospaceCode: { fontFamily: "ui-monospace,'Cascadia Code','Source Code Pro',Menlo,Consolas,'DejaVu Sans Mono',monospace" },
+  industrial: { fontFamily: "Bahnschrift,'DIN Alternate','Franklin Gothic Medium','Nimbus Sans Narrow',sans-serif-condensed,sans-serif" },
+  roundedSans: { fontFamily: "ui-rounded,'Hiragino Maru Gothic ProN',Quicksand,Comfortaa,Manjari,'Arial Rounded MT','Arial Rounded MT Bold',Calibri,source-sans-pro,sans-serif" },
+  slabSerif: { fontFamily: "Rockwell,'Rockwell Nova','Roboto Slab','DejaVu Serif','Sitka Small',serif" },
+  antique: { fontFamily: "Superclarendon,'Bookman Old Style','URW Bookman','URW Bookman L','Georgia Pro',Georgia,serif" },
+  didone: { fontFamily: "Didot,'Bodoni MT','Noto Serif Display','URW Palladio L',P052,Sylfaen,serif" },
+  handwritten: { fontFamily: "'Segoe Print','Bradley Hand',Chilanka,TSCu_Comic,casual,cursive" },
 };
