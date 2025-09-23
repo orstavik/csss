25315e58887e79321a8c117e88907ddf9fc171f3
orstavik <orstavik77@gmail.com>
2025-09-23 15:25:00 +0200

    new version

--- csss.js
+++ csss.js
@@ -2351,6 +2351,38 @@ var filters = {
   ...TRANSFORM,
 };
 
+function _boxShadow(ar) {
+  let color, a2;
+  const res = [];
+  for (let a of ar) {
+    if (a.text == "none")
+      res.push("none");
+    else if (a2 = interpretColor(a))
+      color = a2.text;
+    else if (a2 = interpretLength(a))
+      res.push(a2.text);
+    else
+      throw new SyntaxError(`Could not interpret $boxShadow argument: ${a.text}.`);
+  }
+  if (res.length < 2 || res.length > 4)
+    throw new SyntaxError(`$boxShadow requires two, three, or four length arguments.`);
+  color && res.push(color);
+  return res.join(" ");
+}
+
+function boxShadow(ar) {
+  return { boxShadow: _boxShadow(ar) };
+}
+
+function boxShadowInset(ar) {
+  return { boxShadow: "inset " + _boxShadow(ar) };
+}
+
+var boxShadow$1 = {
+  boxShadow,
+  boxShadowInset,
+};
+
 const SHORTS = {
   ...nativeAndMore,
   ...backgrounds,
@@ -2361,6 +2393,7 @@ const SHORTS = {
   ...textDecorations,
   ...border$1,
   ...filters,
+  ...boxShadow$1,
 };
 
 const MEDIA_WORDS = {
@@ -2569,15 +2602,15 @@ const clashOrStack = (function () {
     backgroundClip: ", ",
     backgroundBlendMode: ", ",
     backgroundAttachment: ", ",
-    transition: ",",
-    fontFamily: ",",
-    voiceFamily: ",",
-    textShadow: ",",
-    boxShadow: ",",
-    animation: ",",
-    mask: ",",
-    fontFeatureSettings: ",",
-    willChange: ",",
+    transition: ", ",
+    fontFamily: ", ",
+    voiceFamily: ", ",
+    textShadow: ", ",
+    boxShadow: ", ",
+    animation: ", ",
+    mask: ", ",
+    fontFeatureSettings: ", ",
+    willChange: ", ",
 
     transform: " ",
     filter: " ",
