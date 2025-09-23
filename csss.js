f546f7a3f784c571a68ee81d7a9e75f587aa4ec7
orstavik <orstavik77@gmail.com>
2025-09-23 16:26:16 +0200

    new version

--- csss.js
+++ csss.js
@@ -166,7 +166,7 @@ function doMath(check, func, post, texter, name, args) {
   const nums = computableNumbers(args);
   return nums ?
     post(func(...nums), args[0]) :
-    { type: args[0].type, text: texter(args[0].map(a => a.text), name) };
+    { type: args[0].type, text: texter(args.map(a => a.text), name) };
 }
 
 var Maths = {
@@ -2383,6 +2383,78 @@ var boxShadow$1 = {
   boxShadowInset,
 };
 
+function position(position, ar) {
+  const res = { position };
+  const [pl1, pl2] = origin[extractName(ar)] || ["left", "top"];
+  res[pl1] = extractLengthPercent$1(ar);
+  res[pl2] = extractLengthPercent$1(ar);
+  if (ar.length)
+    throw new SyntaxError(`unknown argument: $position(${ar[0].text}).`);
+  return res;
+}
+
+const absolute = position.bind(null, "absolute");
+const relative = position.bind(null, "relative");
+const fixed = position.bind(null, "fixed");
+const sticky = position.bind(null, "sticky");
+
+const origin = {
+  left: ["left"],
+  right: ["right"],
+  top: ["top"],
+  bottom: ["bottom"],
+  leftTop: ["left", "top"],
+  rightTop: ["right", "top"],
+  leftBottom: ["left", "bottom"],
+  rightBottom: ["right", "bottom"],
+  start: ["insetInlineStart", "insetBlockStart"],
+  end: ["insetInlineEnd", "insetBlockEnd"],
+  startEnd: ["insetInlineStart", "insetBlockEnd"],
+  endStart: ["insetInlineEnd", "insetBlockStart"],
+  startTop: ["insetInlineStart", "top"],
+  endTop: ["insetInlineEnd", "top"],
+  startBottom: ["insetInlineStart", "bottom"],
+  endBottom: ["insetInlineEnd", "bottom"],
+  leftStart: ["left", "insetBlockStart"],
+  rightStart: ["right", "insetBlockStart"],
+  leftEnd: ["left", "insetBlockEnd"],
+  rightEnd: ["right", "insetBlockEnd"],
+};
+
+// container {
+//   anchor-name: --some-name-never-declared;
+// }
+
+
+// item {
+//   position-anchor: --some-name-never-declared;
+//   position: absolute;
+//   left: anchor(right);
+//   bottom: anchor(top);
+// }
+
+// implementation
+// This is essentially a sideways umbrella. $positionAnchor(some-name-never-declared)
+// Then, $position(absolute,anchor(some-name-never-declared,top,left,bottom,right) || leftTop(l,t),etc.) 
+
+function positionAnchor(ar) {
+  const name = extractName(ar);
+  if (!name) throw new SyntaxError(`$positionAnchor argument must always begin with a valid identifier name.`);
+  if (!ar.length) //sideways umbrella 
+    return { "position-anchor": `--${name}` };
+}
+
+
+
+var position$1 = {
+  absolute,
+  relative,
+  fixed,
+  sticky,
+
+  // positionAnchor,
+};
+
 const SHORTS = {
   ...nativeAndMore,
   ...backgrounds,
@@ -2394,6 +2466,7 @@ const SHORTS = {
   ...border$1,
   ...filters,
   ...boxShadow$1,
+  ...position$1,
 };
 
 const MEDIA_WORDS = {
