91b5e11481082f3a94421a4edb1b897194b3c079
orstavik <orstavik77@gmail.com>
2025-10-22 11:43:30 +0200

    starting with tests

--- src/shadows.js
+++ src/shadows.js
@@ -1,5 +1,5 @@
 //todo we could beneficially use the clock 10:30 etc. as directions for both shadows and gradients!!
-import { extractLength, extractColor, extractName, extractRadian, isLength, } from "./func.js";
+import { extractLength, extractColor, extractName, extractRadian, isLength, isNumber } from "./func.js";
 
 // Shadows are handled similarly to transitions. Or even more sematically regulated.
 // There are say 10 different types of SHADES. They specify a lengthFactor, blurFactor, spreadFactor. 
@@ -15,9 +15,19 @@ import { extractLength, extractColor, extractName, extractRadian, isLength, } fr
 // Ie. elements that have the same shadow in lightmode, might have different/no shadows in darkmode, and vice versa.
 
 const SHADES = {
-  ambient: { l: 0.04, b: 0.3, s: 0.24 },
-  soft: { l: 0.1, b: 0.2, s: 0.2 },
-  crisp: { l: 0.2, b: 0.06, s: 0.1 },
+  ambient: { l: 0.2, b: 1.5, s: 1.25 },
+  soft: { l: 0.5, b: 1, s: 0.5 },
+  edgeGlow: { l: 0, b: .5, s: -.2 },
+  normal: { l: 1, b: 1.5, s: 0.75 },
+  medium: { l: 1.5, b: 2, s: 1 },
+  hard: { l: 2, b: 3, s: 1.5 },
+  sharp: { l: 3, b: 4, s: 2 },
+  heavy: { l: 4, b: 5, s: 3 },
+  massive: { l: 6, b: 8, s: 4 },
+  dramatic: { l: 8, b: 10, s: 6 },
+  pronounced: { l: 10, b: 12, s: 8 },
+  subtle: { l: 0.05, b: 0.1, s: 0.1 },
+  delicate: { l: 0.07, b: 0.15, s: 0.15 },
 };
 
 function round(num, places = 2) {
@@ -56,13 +66,13 @@ function parseNamedShadowArgs(args) {
     throw `Unknown $shadow() argument: ${args[0].text}`;;
   if (!(name in SHADES))
     throw `Unknown shadow type: ${name}. Use one of: ${Object.keys(SHADES).join(", ")}`;
-  const length = isLength(args[0]);
+  const length = args.length && (isLength(args[0]) ?? isNumber(args[0]));
   if (length) args.shift();
   const angle = extractRadian(args);
   const color = extractColor(args) ?? `var(--shadowColor, #0003)`;
   if (args.length)
     throw new TypeError("Unknown named $shadow() argument: " + args[0].text);
-  let { num, unit } = length ?? { num: 5 };
+  let { num, unit } = length || { num: 5 };
   if (!unit) {    // default length is plain number? 5 is "normal" => 0.25rem. 1 is very small => 0.05rem. 10 is large => 0.5rem.
     num /= 20;
     unit = "rem";
@@ -70,7 +80,7 @@ function parseNamedShadowArgs(args) {
   const type = SHADES[name];
 
   const rad = angle ?? (Math.PI * .75); //default angle 135deg
-  const x = round(Math.cos(rad) * type.l * num) + unit;
+  const x = -round(Math.cos(rad) * type.l * num) + unit;
   const y = round(Math.sin(rad) * type.l * num) + unit;
   const blur = round(type.b * num) + unit;
   const spread = round(type.s * num) + unit;
