6447e9e47945e4423c7f4f27c3d29eb2f2fb9492
orstavik <orstavik77@gmail.com>
2025-12-17 20:31:22 +0100

    new release

--- .vscode/ddbb.sh
+++ .vscode/ddbb.sh
@@ -14,6 +14,7 @@
 #   --tree-shaking=false
 
 npx rollup --input src/csss.js --file csss.js --format=esm --sourcemap --no-treeshake --preserveEntrySignatures=strict
+npx rollup --input src/auto.js --file auto.js --format=esm --sourcemap --no-treeshake --preserveEntrySignatures=strict
 output_files=$(find . -maxdepth 1 -type f -name "*.js" -o -name "*.js.map")
 git add $output_files
 
--- auto.js
+++ auto.js
@@ -3828,7 +3828,7 @@ function interpretShort(exp) {
 }
 
 const MAGICWORD = `$"'$`;
-function parse(short) {
+function parse$1(short) {
   const clazz = "." + short.replaceAll(/[^a-zA-Z0-9_-]/g, "\\$&");
   short = short.match(/(.*?)\!*$/)[1];
   const { exp, media } = parseMediaQuery(short, MEDIA_WORDS);
@@ -4302,5 +4302,64 @@ function updateStyleText(styleEl) {
 //   }
 // });
 
-export { TYPES, extractShort, extractShortSelector, memoize, parse, removeUnusedShorts, sequence, updateClassList, updateStyleText };
-//# sourceMappingURL=csss.js.map
+const defaultStyleEl = `
+<style id="csss">
+  @layer container, containerDefault, items, itemsDefault, grandItems, grandItemsDefault;
+</style>`;
+
+const parse = memoize(parse$1, 333);
+
+function allDollars() {
+  const res = [];
+  for (let el of document.querySelectorAll('[class*="$"]'))
+    for (let clazz of el.classList)
+      if (clazz.includes("$"))
+        res.push(clazz);
+  return res;
+}
+
+function updateDoc(style, dollars) {
+  for (let clazz of dollars)
+    try {
+      const res = parse(clazz);
+      const i = style.sheet.cssRules.length;
+      for (let { cssText, rule, short } of res) {
+        if (rule && style.shorts.has(rule)) continue;
+        style.sheet.insertRule(cssText, i);
+        rule && style.shorts.add(rule);
+      }
+    } catch (er) {
+      console.error(er);
+    }
+  const txt = [...style.sheet.cssRules].map(r => r.cssText).join("\n\n");
+  if (txt != style.textContent)
+    style.innerHTML = txt;
+}
+
+function sleep(interval) {
+  const timer = interval < 0 ? requestAnimationFrame : setTimeout;
+  return new Promise(r => timer(r, interval));
+}
+
+(async function () {
+  let style, interval;
+  if (import.meta.url) {
+    const sp = new URL(import.meta.url).searchParams;
+    interval = sp.get("interval") ?? 500;
+    const styleParam = sp.get("style") ?? "style";
+    style = document.querySelector(styleParam);
+  }
+  if (!style) {
+    document.head.insertAdjacentHTML("beforeend", defaultStyleEl);
+    style = document.head.lastElementChild;
+  }
+  style.shorts = new Set([...style.sheet.cssRules].map(rule => extractShort(rule)).filter(Boolean));
+  while (true) {
+    await sleep(interval);
+    const dollars = allDollars().filter(clazz => !style.shorts.has(clazz));
+    if (!dollars.length) continue;
+    dollars.forEach(d => style.shorts.add(d));
+    updateDoc(style, dollars);
+  }
+})();
+//# sourceMappingURL=auto.js.map
