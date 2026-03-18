e0b7170e648dcbb7460435849cab32f28311c2ef
orstavik <orstavik77@gmail.com>
2026-03-18 13:25:19 +0100

    cleaning auto test

--- test/puppeteer.js
+++ test/puppeteer.js
@@ -1,3 +1,4 @@
+const ms = require('ms');
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -10,23 +11,23 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const actualLogs = [];
-  page.on('console', msg => actualLogs.push(msg.text()));
+  const passes = [], errors = [];
+  page.on('console', msg => (msg.type() === 'error' ? errors : passes).push(msg.text()));
 
   await page.goto(url, { waitUntil: 'networkidle0' });
   await browser.close();
 
-  const errors = actualLogs.filter(str => str.startsWith("❌"));
-  const passed = actualLogs.filter(str => str.startsWith("✅"));
-  const whitespaceDiff = actualLogs.filter(str => str.startsWith("🟦"));
+
+  const passed = passes.filter(str => str.startsWith("✅"));
+  const whitespaceDiff = passes.filter(str => str.startsWith("🟦"));
 
   if (passed.length)
     console.log(`✅ Passed tests: ${passed.length}`);
   if (whitespaceDiff.length)
     console.log(`🟦 Passed, but with whitespace differences: ${whitespaceDiff.length}`);
   if (errors.length) {
-    console.log(`❌ Failed tests: ${errors.length}`);
-    for (let err of errors)
+    console.log(`❌ Failed tests: ${errors.filter(str => str.startsWith("❌")).length}`);
+    for (let err of errors.filter(str => !str.startsWith("%c%s")))
       console.log(err);
   }
   process.exit(errors.length ? 1 : 0);
