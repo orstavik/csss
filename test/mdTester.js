3c7a3828a7b1a52551b9827ddfeceecd286dcbfe
orstavik <orstavik77@gmail.com>
2026-04-14 14:41:47 +0200

    bugfix

--- test/mdTester.js
+++ test/mdTester.js
@@ -26,7 +26,7 @@ function splitMd(txt) {
 function printShot(shot) {
   return Object.entries(shot).map(([k, { type: t, value: v }]) => {
     k = "**" + k + ":**";
-    k += (v.includes("\n") || v.length > 120) ? "\n" : " ";
+    k += (v.includes("\n") || v[0] === "`" || v.length > 120) ? "\n" : " ";
     v = v.replaceAll(/^\$/g, " $");
     if (["css", "csss", "html", "js"].includes(t)) v = `\`\`\`${t}\n${v}\n\`\`\``;
     return `${k}${v}`;
